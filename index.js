db7b518d53e9ddd8131098bb464cdbeaf2bb2747
SamannoyB <82638503+SamannoyB@users.noreply.github.com>
2022-08-04 18:48:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,31 @@ client.on('ready', () => {
         description: 'check bot latency'
     });
     client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'ass',
+        description: 'random ass image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
         name: 'hentai',
         description: 'random hentai image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'cum',
+        description: 'random cum image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'feet',
+        description: 'random feet image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'gifs',
+        description: 'random gifs'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'masturbation',
+        description: 'random masturbation image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'panties',
+        description: 'random panties image'
+    });client.guilds.cache.get("1001692509296144445").commands.create({
+        name: 'pussy',
+        description: 'random pussy image'
     });
+     
 });
 
 client.on('interactionCreate', async interaction => {
@@ -33,6 +55,49 @@ client.on('interactionCreate', async interaction => {
         const url = await akaneko.nsfw.hentai();
         interaction.reply(url);
     }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "ass") {
+        const url = await akaneko.nsfw.ass();
+        interaction.reply(url);
+    }
 })
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "cum") {
+        const url = await akaneko.nsfw.cum();
+        interaction.reply(url);
+    }
+})
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "feet") {
+        const url = await akaneko.nsfw.feet();
+        interaction.reply(url);
+    }
+})client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "gifs") {
+        const url = await akaneko.nsfw.gifs();
+        interaction.reply(url);
+    }
+})
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "masturbation") {
+        const url = await akaneko.nsfw.masturbation();
+        interaction.reply(url);
+    }
+})
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "panties") {
+        const url = await akaneko.nsfw.panties();
+        interaction.reply(url);
+    }
+})client.on('interactionCreate', async interaction => {
+    if (interaction.commandName === "pussy") {
+        const url = await akaneko.nsfw.pussy();
+        interaction.reply(url);
+    }
+})
+
+
+
 
 client.login(process.env.TOKEN);
